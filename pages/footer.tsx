ce3d892e9057bf0e28c42ad16244df4627f33b52
Karan Kamdar <karan@1martianway.com>
2024-10-09 23:45:45 +0530

    f

--- pages/footer.tsx
+++ pages/footer.tsx
@@ -5,7 +5,8 @@ const Footer = () => {
             <footer className="footer">
                 <div className="container">
                     <p className="text-muted">
-                        &copy; 2023 1 Martian Way Industries Pvt. Ltd
+                        &copy; {new Date().getFullYear()} 1 Martian Way
+                        Industries Pvt. Ltd
                     </p>
                 </div>
             </footer>
